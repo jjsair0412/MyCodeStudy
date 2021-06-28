218f4ad312b02ef506266658dcd81f7d9c8f4b10
jjsair0412 <jjsair0412@naver.com>
2021-06-28 23:31:48 +0900

    array_base study seconde

--- pjtTest/src/pjtTest/array_base_study.java
+++ pjtTest/src/pjtTest/array_base_study.java
@@ -1,5 +1,6 @@
 package pjtTest;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class array_base_study {
@@ -37,7 +38,8 @@ public class array_base_study {
 
 		
 		String[] name = {"박찬호", "이승엽", "박병호", "이병규", "류현진"};
-		int [] score = new int[5];
+		int [] score = new int[5]; 
+		// int형 배열 5개, int하나가 4byte기 때문에 4*5 = 20byte, score는 총 20byte이다.
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.printf("%s의 점수를 입력하세요",name[0]);
@@ -65,5 +67,39 @@ public class array_base_study {
 		System.out.printf("평점 : %.2f",avg);
 		
 		scanner.close();
+		
+		int [] arrAtt1 = {10, 20, 30, 40, 50, 60};
+		int[] arrAtt2 = null;
+		int[] arrAtt3 = null;
+		
+		// 배열의 길이 구하기
+		System.out.println("배열 arrAtt1의 길이 : "+arrAtt1.length);
+		
+		// 배열 요소 출력
+		System.out.println("배열 arrAtt1의 값들 출력 : "+Arrays.toString(arrAtt1));
+		
+		// 배열 요소 복사
+		// 복사하려는 배열을 먼저 적고, 그 배열의 크기를 적는다. 그러면 arrAtt3에 arrAtt1의 값이 복사되어 저장된다.
+		// 복사하여서 같은 값을 가지고 있다 하더라도, 다른 주소를 가지고 있는 것이다. 각각의 배열은 다른 위치에 있다.
+		arrAtt3 = Arrays.copyOf(arrAtt1, arrAtt1.length);
+		
+		// 배열 레퍼런스
+		arrAtt2 = arrAtt1; // arrAtt1의 메모리 주소를 arrAtt2에 저장한다.
+		System.out.println("arrAtt1 : " + arrAtt1);
+		System.out.println("arrAtt2 : " + arrAtt2);
+		System.out.println("arrAtt3 : " + arrAtt3);
+		
+		// 따라서 arrAtt1과 arrAtt2는 같은 주소를 가지고 있기 때문에, 같은 값을 출력한다.
+		System.out.println("arrAtt2의 값들 출력 : " + Arrays.toString(arrAtt2));
+		
+		
+		// 2차원 배열
+		int [][] arrMul = new int [3][2]; // 3행 2열짜리 배열 생성
+		arrMul[0][0] = 10; // 0행 0열에 10 
+		arrMul[0][1] = 20; // 0행 1열에 20
+		arrMul[1][1] = 11; // 1행 1열에 11
+		
+		System.out.println(arrMul[1][1]); // 11출력
+		System.out.println(arrMul[0][0]); // 10출력
 	}
 }
