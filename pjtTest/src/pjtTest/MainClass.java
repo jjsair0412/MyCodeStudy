0a8a2f29a2e80ab6293385e28121c14338ec7d3e
jjsair0412 <jjsair0412@naver.com>
2021-06-28 12:14:17 +0900

    first day

---
+++ pjtTest/src/pjtTest/MainClass.java
@@ -0,0 +1,9 @@
+package pjtTest;
+
+public class MainClass {
+	public static void main(String[] args) { 
+		// 자바 프로그램에서 가장 먼저 실행되는 메소드. Main 메소드부터 프로그램이 시작된다.
+		System.out.println("Hello Java World!!!");
+		
+	}
+}
