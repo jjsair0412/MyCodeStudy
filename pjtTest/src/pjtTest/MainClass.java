381e950aea64bccd39a9a454e5ca82899e279726
jjsair0412 <jjsair0412@naver.com>
2021-06-28 16:33:41 +0900

    second

--- pjtTest/src/pjtTest/MainClass.java
+++ pjtTest/src/pjtTest/MainClass.java
@@ -2,8 +2,13 @@ package pjtTest;
 
 public class MainClass {
 	public static void main(String[] args) { 
-		// 자바 프로그램에서 가장 먼저 실행되는 메소드. Main 메소드부터 프로그램이 시작된다.
-		System.out.println("Hello Java World!!!");
+		int i = 10;
+		int j;
+		j = 100;
+		
+		
+		i = 200; // 다시 초기화되어 i에는 200이 저장됨.
+		System.out.println("i + j = "+(i+j)); // 300 출력.
 		
 	}
 }
