37229d654e75825e82fd9d038738ea1d7bc517a8
jjsair0412 <jjsair0412@naver.com>
2021-06-29 11:27:10 +0900

    loop base study

---
+++ pjtTest/src/pjtTest/loop_study.java
@@ -0,0 +1,35 @@
+package pjtTest;
+
+import java.util.Scanner;
+
+public class loop_study {
+
+	public static void main(String[] args) {
+		System.out.print("(for) INPUT NUMBER : ");
+		Scanner sca = new Scanner(System.in);
+		int inputnum = sca.nextInt();
+		
+		for(int i = 1; i<10; i++) {
+			// i가 1부터 9까지 증가. 
+			System.out.printf("%d * %d = %d\n",inputnum,i,(i*inputnum));
+		}
+		
+		System.out.print("(while) INPUT NUMBER : ");
+		int inputnum2 = sca.nextInt();
+		int i = 1;
+		while(i < 10) {
+			System.out.printf("%d * %d = %d\n",inputnum2,i,(inputnum2*i));
+			i++;
+		}
+	
+		
+		
+		do {
+			// do while문은, while문에 들어있는 조건식이 false이던 true이던 무조건 한번은 실행한다.
+			// 그 후 조건문을 보고 false일경우 반복을 종료한다.
+			System.out.println("무조건 1번은 실행합니다.");
+		}while(false);
+	
+	}
+
+}
