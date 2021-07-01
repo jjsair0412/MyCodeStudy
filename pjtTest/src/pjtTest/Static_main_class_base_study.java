5c43bc1f6cb9daa9e3cc88475d942c86e907410c
jjsair0412 <jjsair0412@naver.com>
2021-07-01 16:57:34 +0900

    Data hiding base study

--- pjtTest/src/pjtTest/Static_main_class_base_study.java
+++ pjtTest/src/pjtTest/Static_main_class_base_study.java
@@ -12,7 +12,6 @@ public class Static_main_class_base_study {
 		String Cusname = sc.next();
 		Static_sub_class_base_study sub = new Static_sub_class_base_study(Cusname);
 		
-		
 		sub.getInfo();
 		
 		System.out.print("두번째 고객 이름을 입력해주세요 : ");
@@ -22,7 +21,9 @@ public class Static_main_class_base_study {
 		Static_sub_class_base_study sub2 = new Static_sub_class_base_study(secondCusname);
 		
 		sub2.getInfo();
-		System.out.println("\n");
+		sub2.setName("홍길동");
+		// get,set 메서드를 활용해서 private로 막아두었던 멤버변수 값을 교체해주었다.
+		sub2.getInfo();
 		
 		System.out.printf("%s 고객님, 얼마 저축하시겠습니까 ? : ", Cusname);
 		int SaveMoney = sc.nextInt();
@@ -33,10 +34,12 @@ public class Static_main_class_base_study {
 		System.out.println("\n");
 
 		System.out.printf("%s 고객님, 얼마 저축하시겠습니까 ? : ",secondCusname);
+		// 아까 키보드로 기입했던 이름이 출력된다.
 		int SaveMoney2 = sc.nextInt();
 		
 		sub2.saveMoney(SaveMoney2);
 		sub2.getInfo();
+		// get,set 메서드로 바꿔주었던 이름 홍길동으로 getinfo 메서드가 출력된다.
 		
 		// 멤버변수에 static 키워드를 사용한다면, 여러 객체가 생성되어서 다른 메모리에 존재하더라도,
 		// static키워드가 붙어있는 멤버변수는 다른 객체에서도 데이터 공유가 가능하다.
--- pjtTest/src/pjtTest/Static_sub_class_base_study.java
+++ pjtTest/src/pjtTest/Static_sub_class_base_study.java
@@ -1,7 +1,11 @@
 package pjtTest;
 
 public class Static_sub_class_base_study {
-	String name;
+//	public String name;
+	// public의 문제점은, 다른 클래스에서 접근이 자유롭다는게 문제가 된다.
+	// 언제어디서든 해당 멤버변수를 변질시킬수 있기 때문이다.
+	// 데이터를 외부로부터 보호하기 위해서 이런 변수는 private로 만들어준다.
+	private String name;
 	static int amount = 0;
 	
 	// 멤버변수에 static 키워드를 사용한다면, 여러 객체가 생성되어서 다른 메모리에 존재하더라도,
@@ -25,5 +29,17 @@ public class Static_sub_class_base_study {
 		System.out.println("고객 이름 : "+ name);
 		System.out.println("계좌 잔액 : "+ amount);
 	}
-	
+
+	// private로 막아놓은 변수는, 외부 클래스에서 접근할 수 없다. 
+	// 그런데 해당 변수값을 외부 클래스에서 접근할 필요가 있을 때, get,set 메서드를 사용한다.
+	public String getName() {
+		// 속성의 데이터를 반환한다.
+		return name;
+	}
+
+	public void setName(String name) {
+		// 속성 데이터를 넣어준다.
+		// get set을 활용했을때의 장점은, 제약조건을 넣어줄 수 있다는 것이다.
+		this.name = name;	
+	}
 }
